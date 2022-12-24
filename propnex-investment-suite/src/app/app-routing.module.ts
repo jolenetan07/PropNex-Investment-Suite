c933e696be83736afa99d7da6e86a25b3567c69f
jolenetan07 <56619993+jolenetan07@users.noreply.github.com>
2022-12-25 06:10:42 +0800

    setup navigation

--- propnex-investment-suite/src/app/app-routing.module.ts
+++ propnex-investment-suite/src/app/app-routing.module.ts
@@ -4,21 +4,19 @@ import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'places', pathMatch: 'full' },
+  { path: '', redirectTo: 'home/tabs/main', pathMatch: 'full' },
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthPageModule) },
-  {
-    path: 'places',
-    loadChildren: () => import('./places/places.module').then(m => m.PlacesPageModule),
-    canLoad: [AuthGuard]
-  },
-  {
-    path: 'bookings',
-    loadChildren: () => import('./bookings/bookings.module').then(m => m.BookingsPageModule),
-    canLoad: [AuthGuard]
-  },
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'stats',
+    loadChildren: () => import('./stats/stats.module').then( m => m.StatsPageModule)
+  },
+  {
+    path: 'settings',
+    loadChildren: () => import('./settings/settings.module').then( m => m.SettingsPageModule)
   }
 ];
 
