9715281f8cbc7b65fbe42594c0de74ce3f52d3fb
jolenetan07 <56619993+jolenetan07@users.noreply.github.com>
2022-12-25 20:35:18 +0800

    create login

--- propnex-investment-suite/src/app/auth/auth.page.ts
+++ propnex-investment-suite/src/app/auth/auth.page.ts
@@ -1,20 +1,60 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
-
+import { LoadingController } from '@ionic/angular';
 import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.page.html',
-  styleUrls: ['./auth.page.scss']
+  styleUrls: ['./auth.page.scss'],
 })
 export class AuthPage implements OnInit {
-  constructor(private authService: AuthService, private router: Router) {}
+  isLoading = false;
+  isLogin = true;
 
-  ngOnInit() {}
+  constructor(
+    private authService: AuthService, 
+    private router: Router,
+    private loadingCtrl: LoadingController
+  ) { }
+
+  ngOnInit() {
+  }
 
   onLogin() {
+    this.isLoading = true;
     this.authService.login();
-    this.router.navigateByUrl('/home/tabs/main');
+    this.loadingCtrl
+      .create({ keyboardClose: true, message: 'Authenticating...' })
+      .then(loadingEl => {
+        loadingEl.present();
+        setTimeout(() => {
+          this.isLoading = false;
+          loadingEl.dismiss();
+          this.router.navigateByUrl('/home/tabs/main');
+        }, 1500);
+    });
+  }
+
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
+    const username = form.value.username;
+    const password = form.value.password;
+    const usertype = form.value.usertype
+    form.reset();
+    console.log(username, password, usertype);
+
+    if (this.isLogin) {
+      // send request to login servers
+    } else {
+      // send request to signup servers
+    }
+  }
+
+  onSwitchAuthMode() {
+    this.isLogin = !this.isLogin;
   }
 }
