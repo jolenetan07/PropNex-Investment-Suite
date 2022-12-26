cc23529a7744fb0c46550d045bdb7682b7a72ddf
jolenetan07 <56619993+jolenetan07@users.noreply.github.com>
2022-12-27 03:52:51 +0800

    update start route

--- propnex-investment-suite/src/app/app-routing.module.ts
+++ propnex-investment-suite/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home/tabs/main', pathMatch: 'full' },
+  { path: '', redirectTo: 'auth', pathMatch: 'full' },
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthPageModule) },
   {
     path: 'home',
