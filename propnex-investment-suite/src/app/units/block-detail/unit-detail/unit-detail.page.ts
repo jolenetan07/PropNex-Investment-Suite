50cf8aad72886c58e8c4e971608a42aa27612e7b
jolenetan07 <56619993+jolenetan07@users.noreply.github.com>
2023-01-15 13:06:32 +0800

    update unit detail page

--- propnex-investment-suite/src/app/units/block-detail/unit-detail/unit-detail.page.ts
+++ propnex-investment-suite/src/app/units/block-detail/unit-detail/unit-detail.page.ts
@@ -10,17 +10,34 @@ import { Place } from 'src/app/home/place.model';
 export class UnitDetailPage implements OnInit {
   places: Place[];
   unitDetails: string[] = [
-    'Unit Number: ',
-    'Project Name: ',
-    'Floor Area (sqm): ',
-    'Date Of Sale: ',
-    'Flat Model: ',
-    'Max Floor: ',
-    'Min Floor: ',
-    'Private/Public: ',
-    'Remaining Lease (2022): ',
-    'Sale price ($): ',
-    'Type: '
+    'Homeowner Race: ',
+    'Country Of Citizenship: ',
+    'Decorative Style: ',
+    'Extension Request: ',
+    'Price: ',
+    'Title Deed: ',
+    'House Age: ',
+    'Building Height: ',
+    'No. Of Units (total): ',
+    'No. Of Units (per floor): ',
+    'Room Type: ',
+    'Area: ',
+    'No Of Rooms: ',
+    'No. Of Toilets: ',
+    'House Structure: ',
+    'Door Facing: ',
+    'Room Orientation: ',
+    'Corner Unit: ',
+    'Ground/Top Floor: ',
+    'Western Sun: ',
+    'Noisy: ',
+    'Refurbished State: ',
+    'Landscape From Window: ',
+    'Bomb Shelter: ',
+    'Open Kitchen: ',
+    'Gas Management Fee: ',
+    'House Tax: ',
+    'Rental Status: '
   ]
 
   constructor(
