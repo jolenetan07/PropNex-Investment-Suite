bfaa781dff4381c129d3bbbab7531078ef196688
Woon Jia Hui <woonjiahui@gmail.com>
2023-01-19 20:48:32 +0800

    add plotly graph w/ dummy data
    added plotly to assets folder, indicated in angular.json scripts

--- propnex-investment-suite/src/app/stats/stats.page.ts
+++ propnex-investment-suite/src/app/stats/stats.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var Plotly: any;
+
 @Component({
   selector: 'app-stats',
   templateUrl: './stats.page.html',
@@ -10,6 +12,28 @@ export class StatsPage implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    var trace1 = {
+      x: ['2020-10-04', '2021-11-04', '2023-12-04'],
+      y: [90, 40, 60],
+      type: 'scatter'
+    };
+
+    var data = [trace1];
+
+    var layout = {
+      title: '',
+      showlegend: false,
+      width: '400',
+      height: '300'
+    };
+
+    var configs = {
+      scrollZoom: true,
+      responsive: true,
+      displaylogo: false
+    }
+
+    Plotly.newPlot('price-trends-graph', data, layout, configs);
   }
 
 }
