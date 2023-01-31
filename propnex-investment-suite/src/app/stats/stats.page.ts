8ffa69ee6d7997cbf795b92069fad14ab494f89c
Woon Jia Hui <woonjiahui@gmail.com>
2023-01-31 18:03:29 +0800

    made graph responsive and customized graph actions

--- propnex-investment-suite/src/app/stats/stats.page.ts
+++ propnex-investment-suite/src/app/stats/stats.page.ts
@@ -18,19 +18,25 @@ export class StatsPage implements OnInit {
       type: 'scatter'
     };
 
-    var data = [trace1];
+    var trace2 = {
+      x: ['2020-10-04', '2021-11-04', '2023-12-04'],
+      y: [20, 10, 15],
+      type: 'bar'
+    };
+
+    var data = [trace1, trace2];
 
     var layout = {
-      title: '',
       showlegend: false,
-      width: '400',
-      height: '300'
+      dragmode: 'pan',
+      margin: {l:25, r:15, t:10, b:20}
     };
 
     var configs = {
-      scrollZoom: true,
+      scrollZoom: false,
       responsive: true,
-      displaylogo: false
+      displaylogo: false,
+      modeBarButtonsToRemove: ['select2d', 'lasso2d', 'zoom2d', 'resetScale2d']
     }
 
     Plotly.newPlot('price-trends-graph', data, layout, configs);
