1b021a7a4ae24ee43e7843f1e20e1659db620b91
jolenetan07 <56619993+jolenetan07@users.noreply.github.com>
2023-02-09 22:43:11 +0800

    update recommendations list to use backend

--- propnex-investment-suite/src/app/home/recommendations/recommendations.page.ts
+++ propnex-investment-suite/src/app/home/recommendations/recommendations.page.ts
@@ -3,6 +3,8 @@ import { HomeService } from '../home.service';
 import { Place } from '../place.model';
 import { SegmentChangeEventDetail } from '@ionic/core';
 import { IonItemSliding } from '@ionic/angular';
+import { fbPostal, fbUser } from 'src/app/auth/firebase.model';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-recommendations',
@@ -10,22 +12,28 @@ import { IonItemSliding } from '@ionic/angular';
   styleUrls: ['./recommendations.page.scss'],
 })
 export class RecommendationsPage implements OnInit {
-  loadedPlaces?: Place[];
-  displayedPlaces?: Place[];
+  currUser: fbUser;
+  //loadedPlaces?: Place[];
+  //displayedPlaces?: Place[];
+  loadedPlaces?: fbPostal[];
+  displayedPlaces?: fbPostal[];
   //result;
 
-  constructor(private homeService: HomeService) { }
+  constructor(
+    private homeService: HomeService,
+    private authService: AuthService,
+  ) { }
 
   ngOnInit() {
-    this.loadedPlaces = this.homeService.personalRecPlaces;
+    this.loadedPlaces = this.authService.currFbUser.personalRec;
     this.displayedPlaces = this.loadedPlaces;
   }
 
   onFilterUpdate(event: CustomEvent<SegmentChangeEventDetail>) {
     if (event.detail.value === 'personal') {
-      this.displayedPlaces = this.homeService.personalRecPlaces;
+      this.displayedPlaces = this.authService.currFbUser.personalRec;
     } else {
-      this.displayedPlaces = this.homeService.trendRecPlaces;
+      this.displayedPlaces = this.authService.currFbUser.generalRec;
     }
   }
 
